7e7836295dcd65338dbfca76e5aa2c439fe7a87e
CarlosAugustoP <capv2004@gmail.com>
2025-05-15 20:07:41 -0300

    done

--- h-m-front/src/Pages/Home/index.js
+++ h-m-front/src/Pages/Home/index.js
@@ -4,17 +4,20 @@ import Header from "../../Components/Header";
 import HeartRateChart from "../../Components/HeartRateChart";
 import Latest from "../../Components/Latest";
 import styles from "./Home.module.css";
+import '../../index.css';
 
 export default function Home() {
   return (
     <>
       <div className={styles.container}>
         <Header />
-        <div className={styles.content}>
-          <HeartRateChart />
+        <div style={ {padding: "30px",} }>
+            <HeartRateChart />
+          <Latest />
+          <AllReadings />
+          <Footer />
+                </div>
         </div>
-        <Footer />
-      </div>
     </>
   );
 }
